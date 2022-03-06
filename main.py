4f37cfbc88768397eb95af95e080fb77988ef6e9
wolf <maxmixf@gmail.com>
2022-03-06 11:05:17 +0300

    error with xcor and ycor

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import turtle
 from random import randint
-import time
-
 
 def right():
     turtle.setheading(0)
@@ -19,20 +17,14 @@ def bottom():
     turtle.setheading(270)
     turtle.fd(100)
 
-def apple():
-    turtle.color(200, 50, 2)
-    turtle.penup()
-    x = randint(-display_width / 2, display_width / 2)
-    y = randint(-display_height / 2, display_height / 2)
-    turtle.goto(x, y)
-    turtle.pendown()
-    turtle.begin_fill()
-    turtle.circle(10)
-    turtle.end_fill()
-
 
 def balls():
     print(turtle.position())
+
+
+def goto():
+    turtle_pos = turtle.position()
+    return turtle_pos
 
 
 # цвета и скорость
@@ -48,24 +40,51 @@ display.setup(display_width, display_height)
 display.bgcolor(30, 50, 0)
 
 # рисование яблок
-apple()
-x_y_1 = turtle.position()
-apple()
-x_y_2 = turtle.position()
-apple()
-x_y_3 = turtle.position()
+apple_1 = turtle.Turtle()
+apple_2 = turtle.Turtle()
+apple_3 = turtle.Turtle()
+apple_1.shape('circle')
+apple_2.shape('circle')
+apple_3.shape('circle')
+apple_2.color('red')
+apple_1.color('red')
+apple_3.color('red')
 
+apple_1.penup()
+apple_2.penup()
+apple_3.penup()
+x = randint(-display_width / 2,display_width / 2)
+y = randint(-display_height / 2, display_height / 2)
+apple_1.goto(x, y)
+x = randint(-display_width / 2, display_width / 2)
+y = randint(-display_height / 2, display_height / 2)
+apple_2.goto(x, y)
+x = randint(-display_width / 2, display_width / 2)
+y = randint(-display_height / 2, display_height / 2)
+apple_3.goto(x, y)
 
 # курсор из черепашки
 turtle.color(255, 182, 117)
 turtle.shape('circle')
 
-# передвижение
-turtle.penup()
+#подготовка к while
 turtle.goto(0, 0)
 ball = 0
-turtle.onscreenclick(turtle.goto)
+turtle.penup()
 
+# передвижение
+
+turtle.onscreenclick(turtle.goto)
+x_range = range(apple_1.xcor() - 10, apple_1.xcor() + 10)
+y_range = range(apple_1.ycor() - 10, apple_1.ycor() + 10)
+apple_pos = x_range, y_range
+x_t = range(turtle.xcor() - 10, turtle.xcor() + 10)
+y_t = range(turtle.ycor() - 10, turtle.ycor() + 10)
+turtle_pos = x_t, y_t
+if turtle_pos in apple_pos:
+    ball += 1
+if ball == 1:
+    print('УРА ПОБЕДА')
 
 turtle.listen()
 turtle.mainloop()
